044f358c255a94cefd9bbb5335e4fbf021b28ca5
weihang <pehweihang@live.com>
2023-11-17 06:26:05 +0800

    restructure done

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 import random
+import re
 import string
-from typing import List
+from typing import TYPE_CHECKING, Any, Dict, List, Set
+
+if TYPE_CHECKING:
+    from explore import Node
 
 
 def build_select(relation: str, conditions: List[str]) -> str:
@@ -71,3 +77,123 @@ def random_string(n: int = 5) -> str:
         Random string of length n
     """
     return "".join(random.choices(string.ascii_uppercase, k=n))
+
+
+def get_aliases_in_condition(cond: str) -> Set[str]:
+    """
+    Extract the aliases in a condition
+
+    Args:
+        cond: condition to extract
+
+    Returns:
+        set of aliases
+
+    Examples:
+        >>> get_aliases_in_condition("(table_a.id = table_b.id)")
+        {"table_a", "table_b"}
+    """
+    return set(re.findall(r"([a-zA-Z_]+)\.[a-zA-Z_]+", cond))  # type: Set[str]
+
+
+class ViewNotFoundException(Exception):
+    pass
+
+
+def replace_aliases_with_views(statement: str, views: Dict[str, str]) -> str:
+    """
+    Replace aliases in a statement with corresponding views
+
+    Args:
+        statement: statement to replace
+        view: dictionary mapping aliases to view names
+
+    Returns:
+        altered statement with aliases replaced with view names
+
+    Examples:
+        >>> views = {"table_a": "view_a", "table_b": "view_b"}
+        >>> replace_aliases_with_views("(table_a.id = table_b.b_id)")
+        "(view_a.id = view_b.bid)"
+    """
+    aliases = get_aliases_in_condition(statement)
+    for alias in aliases:
+        statement = re.sub(rf"{alias}\.", f"{views[alias]}.", statement)
+    return statement
+
+
+def condition_is_join(cond: str) -> bool:
+    """
+    Checks if a condition is a JOIN condition
+
+    Args:
+        cond: condition to check
+
+    Returns:
+        True if condition is a join else False
+
+    Examples:
+        >>> condition_is_join("(table_a.id = table_b.id)")
+        True
+
+        >>> condition_is_join("(table_a.id > 50)")
+        False
+    """
+
+    return (
+        re.search(
+            r"[a-zA-Z_]+\.?[a-zA-Z_]+ = ([a-zA-Z_]+)\.[a-zA-Z_]+",
+            cond,
+        )
+        is not None
+    )
+
+
+def alter_join_condition(cond: str) -> str:
+    """
+    Alter JOIN condition in WHERE clause
+
+    Args:
+        cond: condition to alter
+
+    Returns:
+        Altered condition
+
+    Examples:
+        >>> alter_join_condition("(table_a.id = table_b.b_id)")
+        "(table_a.id IN (SELECT b_id from table_b))"
+    """
+    cond_attributes = re.search(
+        r"(?P<left>[a-zA-Z_]+\.?[a-zA-Z_]+) = "
+        r"(?P<table_right>[a-zA-Z_]+)\.(?P<right_column>[a-zA-Z_]+)",
+        cond,
+    )
+    if cond_attributes is None:
+        raise ValueError(
+            f"Unable to find join attributes in condition: {cond}"
+        )
+    left = cond_attributes.group("left")
+    table_right = cond_attributes.group("table_right")
+    right_column = cond_attributes.group("right_column")
+    return "( {} IN (SELECT {} FROM {}) )".format(
+        left, right_column, table_right
+    )
+
+
+def get_child_with_attribute(
+    node: Node, attr_name: str, attr_val: Any
+) -> Node | None:
+    """
+    Get the first child node with an attribute having a certain value
+
+    Args:
+        node: parent node
+        attr_name: name of attribute
+        attr_val: value of attribute to match
+
+    Returns:
+        first node found else None
+    """
+    return next(
+        child for child in node.children if child[attr_name] == attr_val
+    )
